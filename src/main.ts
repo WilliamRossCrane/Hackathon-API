09cbfbe37eaaad4ef71a65392bb629f449b79362
William Crane <wcran7@gmail.com>
2026-08-26 13:09:02 +1000

    feat(Validator): Ran out of credits... annoying part of figuring out how to use AI for coding

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import { ValidationError } from 'class-validator';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
@@ -6,6 +8,21 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bodyParser: false,
   });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      exceptionFactory: (errors: ValidationError[]) =>
+        new BadRequestException(
+          errors.flatMap(({ property, constraints }) =>
+            Object.values(constraints ?? {}).map((message) => ({
+              property,
+              message,
+            })),
+          ),
+        ),
+    }),
+  );
   app.useGlobalInterceptors(app.get(TransformInterceptor));
   await app.listen(process.env.PORT ?? 3000);
 }
